f9a4e0274c21c7af762605d584ca6bd22a1b4b7d
Edgar Andreé Toledo Torres <edgar.andree20@gmail.com>
2020-05-12 16:45:37 -0600

    Composer Controller Added
    Create Controler for Composer

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,38 +4,10 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable {
-	use Notifiable;
-	protected $table = 'users';
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'name', 'email', 'password',
-	];
 
-	/**
-	 * The attributes that should be hidden for arrays.
-	 *
-	 * @var array
-	 */
-	protected $hidden = [
-		'password', 'remember_token',
-	];
-
-	/**
-	 * The attributes that should be cast to native types.
-	 *
-	 * @var array
-	 */
-	protected $casts = [
-		'email_verified_at' => 'datetime',
-	];
-
-	public function worker() {
-		$this->hasOne('App\Models\Worker');
-	}
+	use HasApiTokens, Notifiable;
+	
 }
