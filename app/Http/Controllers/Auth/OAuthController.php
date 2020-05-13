2ef42821ff12ecb1271ad7be92acbc661162e537
Augusto Alonso <augustoalonso98@gmail.comn>
2020-05-12 22:42:41 -0600

    fixes

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -1,61 +1,68 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
 
-use Laravel\Passport\Http\Controllers\AccesTokenController;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
 use Psr\Http\Message\ServerRequestInterface;
+use Illuminate\Http\Request;
+use Exception;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use League\OAuth2\Server\Exception\OAuthServerException;
 
-class AuthController extends AccesTokenController
+class OAuthController extends AccessTokenController
 {
-    /**
-     * Function of AuthController
-     */
-    use ProvidesConvenienceMethods;
-    
-    public function login(ServerRequestInterface $request){
-        try{
+
+    public function login(ServerRequestInterface $request)
+    {
+        try {
             $tokenResponse = parent::issueToken($request);
 
             $token = $tokenResponse->getContent();
 
             $tokenInfo = json_decode($token, true);
 
-            if (array_key_exists('error', $tokenInfo)){
+            if (array_key_exists('error', $tokenInfo)) {
 
                 return response($tokenInfo, 401);
-
             }
 
             $request = $request->getParsedBody();
 
             $username = $request['username'];
 
-            $user     = User::where('email',$email)->first();
+            $user     = User::where('email', $email)->first();
 
             $tokenInfo = collect($tokenInfo);
 
-            $tokenInfo->put('user',$user);
+            $tokenInfo->put('user', $user);
 
 
             return $tokenInfo;
-            
-        } catch (ModelNotFoundException $e){
+        } catch (ModelNotFoundException $e) {
             //email not found
             //return error message
-            return response(["mensaje"=>"Usuario no encontrado"], 500);
-        
-        } catch (OAuthServerException $e){
+            return response(["mensaje" => "Usuario no encontrado"], 500);
+        } catch (OAuthServerException $e) {
             //password no correct
             //return error message
-            return response(["mensaje"=>"El usuario uso credenciales no correctas,', 6, 'invalid_credentials"], 500);
-        
-        } catch (Exception $e){
+            return response(["mensaje" => "El usuario uso credenciales no correctas,', 6, 'invalid_credentials"], 500);
+        } catch (Exception $e) {
             //return error message
-            return response(["mensaje"=>"Internal server error"], 500);
-
+            return response(["mensaje" => "Internal server error"], 500);
         }
     }
 
-
-
+    /**
+     * Logout user (Revoke the token)
+     *
+     * @return [string] message
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
     }
-    
+}
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,53 +33,14 @@ class AuthController extends Controller
         return response()->json(["message", "successfully created"]);
     }
 
-    /**
-     * Login user and create token
-     *
-     * @param  [string] email
-     * @param  [string] password
-     * @param  [boolean] remember_me
-     * @return [string] access_token
-     * @return [string] token_type
-     * @return [string] expires_at
-     */
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-            'remember_me' => 'boolean'
-        ]);
-        $credentials = request(['email', 'password']);
-        if (!Auth::attempt($credentials))
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
-        $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
-        $token->save();
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
-        ]);
-    }
+        $userCredentials = $request->only('email', 'password');
 
-    /**
-     * Logout user (Revoke the token)
-     *
-     * @return [string] message
-     */
-    public function logout(Request $request)
-    {
-        $request->user()->token()->revoke();
-        return response()->json([
-            'message' => 'Successfully logged out'
-        ]);
+        if (Auth::attempt($userCredentials)) {
+            return response()->json(["Authenticated"], 200);
+        } else {
+            return response()->json(["Invalid Credentials"], 411);
+        }
     }
 }
