6ea7897787ddcf423bc361dd00a26661d687400c
JavierRamirezCospin <ram18099@uvg.edu.gt>
2020-05-13 19:16:31 -0500

    Records queries
    Queries to show all Records, or show by username, package code bar or modification date

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -7,4 +7,47 @@ use App\Models\Records;
 
 class RecordsController extends Controller
 {
+    //Muestra todo el historial de la aplicacion
+    public function getAllRecords() 
+    {
+        $records = Records::
+            join('workers', 'workers.id', '=', 'records.worker_id')
+            ->join('packages', 'packages.id', '=', 'records.package_id')
+            ->select('workers.name', 'packages.bar_code', 'records.old_locker', 'records.new_locker', 'records.updated_at')
+            ->get();
+    }
+    //Recibe: Nombre de usuario
+    //Muestra todos los cambios realizados por el usuario en el sistema
+    public function getRecordsByUserName($username) 
+    {
+        $records = Records::
+            join('workers', 'workers.id', '=', 'records.worker_id')
+            ->join('packages', 'packages.id', '=', 'records.package_id')
+            ->select('workers.name', 'packages.bar_code', 'records.old_locker', 'records.new_locker', 'records.updated_at')
+            ->where('workers.name', '=', $username)
+            ->get();
+    }
+    //Recibe: Codigo de barras
+    //Muestra todos los cambios hechos para un empaque en particular
+    public function getRecordsByCodeBar($barcode)
+    {
+        $records = Records::
+            join('workers', 'workers.id', '=', 'records.worker_id')
+            ->join('packages', 'packages.id', '=', 'records.package_id')
+            ->select('workers.name', 'packages.bar_code', 'records.old_locker', 'records.new_locker', 'records.updated_at')
+            ->where('packages.bar_code', '=', $barcode)
+            ->get();
+    }
+    //Recibe: fecha de modificiacion
+    //Muestra todos los cambios realizados en cierta fecha
+    public function getRecordsByDate($modificationDate)
+    {
+        $records = Records::
+            join('workers', 'workers.id', '=', 'records.worker_id')
+            ->join('packages', 'packages.id', '=', 'records.package_id')
+            ->select('workers.name', 'packages.bar_code', 'records.old_locker', 'records.new_locker', 'records.updated_at')
+            ->where('records.created_at', '=', $modificationDate)
+            ->orWhere('records.updated_at', '=', $modificationDate)
+            ->get();
+    }
 }
