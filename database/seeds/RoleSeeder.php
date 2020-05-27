4cf1759bcae7e8998673f0990a3d6ecc41822f9a
JavierRamirezCospin <ram18099@uvg.edu.gt>
2020-05-27 10:26:19 -0500

    Update RoleSeeder

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -14,18 +14,11 @@ class RoleSeeder extends Seeder
     public function run()
     {
         $now = Carbon::now();
-        //Permisos para Trabajadores y Cuadradores de Empaques
-        Role::insert(['name' => 'Ingresar empaques', 'created_at' => $now, 'updated_at' => $now]);
-        Role::insert(['name' => 'Modificar ubicacion de empaques', 'created_at' => $now, 'updated_at' => $now]);
-        Role::insert(['name' => 'Despachar empaques', 'created_at' => $now, 'updated_at' => $now]);
-        //Permisos para Admnistrador
-        Role::insert(['name' => 'Agregar Usuarios', 'created_at' => $now, 'updated_at' => $now]);
-        Role::insert(['name' => 'Eliminar Usuarios', 'created_at' => $now, 'updated_at' => $now]);
-        //Permisos para Jefes
-        Role::insert(['name' => 'Convertir Data en archivo PDF', 'created_at' => $now, 'updated_at' => $now]);
-        Role::insert(['name' => 'Convertir Data en archivo XLS', 'created_at' => $now, 'updated_at' => $now]);
-        //Permisos para Todos los Usuarios
-        Role::insert(['name' => 'Ver Historial', 'created_at' => $now, 'updated_at' => $now]);
-        Role::insert(['name' => 'Realizar Busqueda Detallada', 'created_at' => $now, 'updated_at' => $now]);
+        //Permisos para Trabajadores y Cuadradores de Empaques: Ingresar empaques, modificar ubicacion de empaques, despachar empaques y ver historial
+        Role::insert(['name' => 'Trabajador', 'created_at' => $now, 'updated_at' => $now]);        
+        //Permisos para Admnistrador: Agregar usuarios, quitar usuarios, ver historial y realizar busqueda detallada
+        Role::insert(['name' => 'Administrador', 'created_at' => $now, 'updated_at' => $now]);
+        //Permisos para Jefes: Convertir queries a formatos pdf o xls, ver historial y realizar busqueda detallada
+        Role::insert(['name' => 'Jefe', 'created_at' => $now, 'updated_at' => $now]);
     }
 }
