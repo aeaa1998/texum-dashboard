7472456be9e8f1553677c3bdb32eac496dd16d16
JavierRamirezCospin <ram18099@uvg.edu.gt>
2020-09-03 23:17:59 -0600

    Email Function created

---
+++ resources/views/emails/WelcomeEmail.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Mensaje de Bienvenida</title>
+</head>
+<body>
+    <p> Bienvenido {{ $msg['first_name']}} {{ $msg['last_name']}}</p>
+    <p></p>
+    <p> Por favor espera a que uno de nuestros adminsitradores te acepte al sistema.</p>
+</body>
+</html>
