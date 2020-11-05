5304ea340b58a8a9cbbb3a34610c4183e695c084
Edgar Andreé Toledo Torres <edgar.andree20@gmail.com>
2020-11-05 16:10:24 -0600

    Update WorkersControllerTest.php
    Unit test second, run time 3.76s

--- tests/Unit/WorkersControllerTest.php
+++ tests/Unit/WorkersControllerTest.php
@@ -5,22 +5,42 @@ namespace Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Database\Eloquent\FactoryBuilder;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Request;
 use Tests\TestCase;
+use App\Models\User;
 use App\Models\Worker;
+use Carbon\Carbon;
+
+
 
 class RequestWorkersController extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      *
      * @return void
      */
+
+     private $user= null;
+
+     public function setUp() : void
+     {
+         parent::setUp();
+         Schema::disableForeignKeyConstraints();
+         factory(Worker::class)->create();
+         $this->user = User::first();
+         $this->user->verified_at = Carbon::now();
+         Schema::enableForeignKeyConstraints();
+         
+     }
+
     public function testGetWorkersController()
     {
-      
-        $response = $this->get('requests/general');
-        
-        $response->assertStatus(302)->assertJson(["message", "successfully"]);
+        $response = $this->actingAs($this->user)
+                        ->get('/requests/general');
+        $response->assertStatus(200);
     }
 }
